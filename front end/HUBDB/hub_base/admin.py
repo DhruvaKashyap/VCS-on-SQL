ced10a94305228b0df51163e44b84d0271270689
Dhruva Kashyap <dhruva12kashyap@gmail.com>
2020-05-29 14:58:49 +0530

    final

--- front end/HUBDB/hub_base/admin.py
+++ front end/HUBDB/hub_base/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import *
+#from django.contrib.admin.models import LogEntry
 
+#LogEntry.objects.all().delete()
 # Register your models here.
 admin.site.register(Coder)
 admin.site.register(Commits)
---
+++ queries.sql
@@ -0,0 +1,5 @@
+\c hub
+SELECT file_name,contributor from files WHERE repo_name='android';
+SELECT fname,lname FROM employee NATURAL JOIN server WHERE location='London';
+
+
